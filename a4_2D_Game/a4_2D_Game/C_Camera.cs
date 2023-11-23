21a950e9b5439a6bf069159c9bb541bf73332b3a
cshort4403 <christopher.short1@mohawkcollege.ca>
2023-11-23 16:48:15 -0500

    Create camera class

---
+++ a4_2D_Game/a4_2D_Game/C_Camera.cs
@@ -0,0 +1,40 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a4_2D_Game
+{
+	internal class C_Camera : Component
+	{
+		public static E_ComponentID id = E_ComponentID.C_CAMERA;
+
+		public Camera2D camera;
+		
+		public float zoom = 1f;
+
+		public C_Camera(Object parentObj) : base(id, parentObj)
+		{
+			
+		}
+
+		public override void Load()
+		{
+			camera.Offset = new Vector2(0, -300);
+			camera.Target = GetParentObject().position;
+			camera.Zoom = zoom;
+			
+			base.Load();
+		}
+
+		public override void Update()
+		{
+			camera.Target = GetParentObject().position;
+			base.Update();
+		}
+
+	}
+}
